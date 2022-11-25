cae82ffdb25a4c297456c7e49c72cba636de3ba2
Garrett <gcamoin5@gmail.com>
2022-11-25 14:56:11 -0500

    commit tree e276fa0dc378591675bddbb2cd0fe431a70b3f34

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+const names = ["Charlie", "Samip", "Ali"];
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+    console.log(`Thank you, ${names[i]} for the wonderful birthday gift!`);
+    i++;
+}
+return writeCards;
+}
